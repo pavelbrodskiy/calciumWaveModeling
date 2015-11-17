a29b60a1ca3221dc8e8aa5f2be3fecda8c890c2f
pavelbrodskiy <pavelbrodskiy@gmail.com>
2015-11-17 00:36:01 -0500

    cleaned up and sped up eigenmode code

--- signalAnalysis/eigenmodeAnalysis.m
+++ signalAnalysis/eigenmodeAnalysis.m
@@ -3,10 +3,13 @@ clc;
 clear all;
 close all;
 
+%% Parameters
 discs = [1:5, 12:17, 21, 22, 24:31];
 mmm = 1;
-
+K=8;
+    
 for kkk = discs
+    %% Read in tiff stack
     filename = ['rawDiscData2/Raw/'  num2str(kkk,'%03d') '.tif'];
     imageInfo = imfinfo(filename);
     numFrames = length(imageInfo);
@@ -16,37 +19,17 @@ for kkk = discs
         X(:,:,frame) = imread(filename,frame);
     end
     
-    % Generate background image as the minimum luminosity
-    Xdark=min(X,[],3);
-    
-    Y=X-repmat(Xdark,[1,1,361]);
-    
     %% Subtract mean image for zero-mean deviation
-    [h,w,nFrames]=size(Y);
-    W=zeros(h,w,nFrames);
-    Ymean=mean(Y,3);
-    for n=1:nFrames
-        Ws(:,:,n)=Y(:,:,n)-Ymean;
+    [~,~,nFrames]=size(X);
+    Xmean=mean(X,3);
+    for n=nFrames:-1:1
+        Ws(:,:,n)=X(:,:,n)-Xmean;
     end
     
     %% Resize via low-pass filtering for SVD analysis
-    
-    K=8;
-    
-    [h,w,nFrames]=size(Ws);
-    
-    A=zeros(nFrames,h*w);
-    for n=1:nFrames
-        x=Ws(:,:,n);
-        A(n,:)=x(:)';
-    end
+    A=permute(Ws,[2,1,3]);
     
     [u,s,v]=svds(A,K); % s is the singular value matrix, v is the eigenmode?
-    
-    weightArray = s(:);
-    weightArray (weightArray == 0) = [];
-    weights{mmm} = weightArray;
-    mmm = mmm + 1;
     
     V=zeros(h,w,nFrames);
     U=zeros(nFrames,K);
@@ -58,14 +41,13 @@ for kkk = discs
     
     close all
     figure;
-    subplot(2,4,1);
     for k=1:K
         subplot(2,4,k);
-        imshow(0.5+20*V(:,:,k));
+        imshow(V(:,:,k),[min(V) max(V)]);
         title(sprintf('k=%d',k));
     end
     
-    print(['Eigenmodes ' num2str(discs(kkk), '%03d')],'-painters', '-dpdf', '-r1200')
+    print(['Eigenmodes ' num2str(kkk, '%03d')],'-painters', '-dpdf', '-r1200')
     
 end
 
